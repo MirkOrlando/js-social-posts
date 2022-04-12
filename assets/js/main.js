62bd763c04b5357b2c9ca80b366e8aae7238611c
MirkOrlando <mirko@orlando-m.it>
2022-04-12 15:26:23 +0200

    created css card structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,14 +10,63 @@ immagine (non tutti i post devono avere una immagine),
 numero di likes.
 Non è necessario creare date casuali
 Per le immagini va bene utilizzare qualsiasi servizio di placeholder 
-ad es. Unsplash (https://unsplash.it/300/300?image=<id>) 
+ad es. Unsplash (https://unsplash.it/300/300?image=) 
 */
+
+const posts = [
+  {
+    id: 1,
+    nicknameAuthor: "Mirko Orlando",
+    proPicAuthor: "https://picsum.photos/300",
+    date: "04/12/2022",
+    image: "https://picsum.photos/300",
+    likes: 22,
+  },
+  {
+    id: 2,
+    nicknameAuthor: "Stefano Russo",
+    proPicAuthor: "https://picsum.photos/300",
+    date: "04/10/2022",
+    image: "https://picsum.photos/300",
+    likes: 275,
+  },
+  {
+    id: 3,
+    nicknameAuthor: "Matteo Gramegna",
+    proPicAuthor: "https://picsum.photos/300",
+    date: "04/07/2022",
+    image: "https://picsum.photos/300",
+    likes: 184,
+  },
+  {
+    id: 4,
+    nicknameAuthor: "Lucia Verdi",
+    proPicAuthor: "https://picsum.photos/300",
+    date: "03/31/2022",
+    image: "https://picsum.photos/300",
+    likes: 98,
+  },
+  {
+    id: 5,
+    nicknameAuthor: "Clara Bianchi",
+    proPicAuthor: "https://picsum.photos/300",
+    date: "03/27/2022",
+    image: "https://picsum.photos/300",
+    likes: 67,
+  },
+];
+
+console.log(posts);
 
 /* 
 Milestone 2
 Prendendo come riferimento il layout di esempio presente nell'html,
 stampiamo i post del nostro feed.
 */
+// 1. inizializzare la variabile per l'elemento della dom in cui verrà inserito il post
+// 2. ciclare l'array per lavorare su ogni singolo post
+// 3. creare la card del post
+// 4. appendere le card nel container
 
 /*
 Milestone 3
