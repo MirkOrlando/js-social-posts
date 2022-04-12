be865cd4b50f053332fc20592d0c7d6ba3fae5fd
MirkOrlando <mirko@orlando-m.it>
2022-04-12 15:44:28 +0200

    inserted cards dynamically

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 /* Milestone 1
 Creiamo il nostro array di oggetti che rappresentano ciascun post. 
-Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+Ogni post dovrà avere le informazioni necessarie per stampare 
+la relativa card:
 id del post, numero progressivo da 1 a n
 nome autore,
 foto autore,
@@ -19,7 +20,9 @@ const posts = [
     nicknameAuthor: "Mirko Orlando",
     proPicAuthor: "https://picsum.photos/300",
     date: "04/12/2022",
-    image: "https://picsum.photos/300",
+    caption:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibu corrupti ad mollitia minima, placeat vel maxime perspiciatis, alias velit quasi, doloribus aliquid doloremque sint at ea impedit voluptates repellat voluptatum.",
+    image: "https://picsum.photos/600/300",
     likes: 22,
   },
   {
@@ -27,7 +30,9 @@ const posts = [
     nicknameAuthor: "Stefano Russo",
     proPicAuthor: "https://picsum.photos/300",
     date: "04/10/2022",
-    image: "https://picsum.photos/300",
+    caption:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibu corrupti ad mollitia minima, placeat vel maxime perspiciatis, alias velit quasi, doloribus aliquid doloremque sint at ea impedit voluptates repellat voluptatum.",
+    image: "https://picsum.photos/600/300",
     likes: 275,
   },
   {
@@ -35,7 +40,9 @@ const posts = [
     nicknameAuthor: "Matteo Gramegna",
     proPicAuthor: "https://picsum.photos/300",
     date: "04/07/2022",
-    image: "https://picsum.photos/300",
+    caption:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibu corrupti ad mollitia minima, placeat vel maxime perspiciatis, alias velit quasi, doloribus aliquid doloremque sint at ea impedit voluptates repellat voluptatum.",
+    image: "https://picsum.photos/600/300",
     likes: 184,
   },
   {
@@ -43,7 +50,9 @@ const posts = [
     nicknameAuthor: "Lucia Verdi",
     proPicAuthor: "https://picsum.photos/300",
     date: "03/31/2022",
-    image: "https://picsum.photos/300",
+    caption:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibu corrupti ad mollitia minima, placeat vel maxime perspiciatis, alias velit quasi, doloribus aliquid doloremque sint at ea impedit voluptates repellat voluptatum.",
+    image: "https://picsum.photos/600/300",
     likes: 98,
   },
   {
@@ -51,7 +60,9 @@ const posts = [
     nicknameAuthor: "Clara Bianchi",
     proPicAuthor: "https://picsum.photos/300",
     date: "03/27/2022",
-    image: "https://picsum.photos/300",
+    caption:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibu corrupti ad mollitia minima, placeat vel maxime perspiciatis, alias velit quasi, doloribus aliquid doloremque sint at ea impedit voluptates repellat voluptatum.",
+    image: "https://picsum.photos/600/300",
     likes: 67,
   },
 ];
@@ -63,10 +74,37 @@ Milestone 2
 Prendendo come riferimento il layout di esempio presente nell'html,
 stampiamo i post del nostro feed.
 */
-// 1. inizializzare la variabile per l'elemento della dom in cui verrà inserito il post
+// 1. inizializzare la variabile per l'elemento della dom in cui
+//      verrà inserito il post
+const containerElement = document.querySelector(".container");
 // 2. ciclare l'array per lavorare su ogni singolo post
-// 3. creare la card del post
-// 4. appendere le card nel container
+posts.forEach((post) => {
+  // 3. creare la card del post
+  const cardElement = `
+        <div class="card">
+            <div class="author">
+                <div class="propic_container">
+                    <img src="${post.proPicAuthor}" alt="" />
+                </div>   
+                <div class="nickname_container">
+                    <div class="nickname">${post.nicknameAuthor}</div>
+                    <div class="date">${post.date}</div>
+                </div>
+            </div>
+            <div class="caption">${post.caption}</div>
+            <img src="${post.image}" alt="" />
+            <div class="like_action">
+                <div class="btn_like">
+                    <i class="fa-solid fa-thumbs-up"></i> Mi Piace
+                </div>
+                <div class="likes_counter">
+                    Piace a <span class="n_likes">${post.likes}</span> persone
+                </div>
+            </div>
+        </div>`;
+  // 5. appendere le card nel container
+  containerElement.insertAdjacentHTML("beforeend", cardElement);
+});
 
 /*
 Milestone 3
